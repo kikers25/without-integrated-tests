351e32052d9f12ca4a6bf73756414dc8af021b0e
Enrique Martin <kikers25@gmail.com>
2014-08-30 18:48:24 +0200

    Added test: accelerometer can respond to failure when reporting acceleration

--- src/test/java/com/mars/rover/AccelerometerTests.java
+++ src/test/java/com/mars/rover/AccelerometerTests.java
@@ -1,22 +1,45 @@
 package com.mars.rover;
 
+import static org.junit.Assert.fail;
+
 import org.jmock.Expectations;
 import org.jmock.auto.Mock;
 import org.junit.Test;
 
 public class AccelerometerTests extends TestsWithMokingClasses {
-	
-    @Mock
-    private AccelerationObserver observer;
-    
+
+	@Mock
+	private AccelerationObserver observer;
+
 	@Test
-    public void testAccelerometerCanReportRapidAcceleration() throws Exception {
+	public void testAccelerometerCanReportRapidAcceleration() throws Exception {
 		Accelerometer accelerometer = new Accelerometer();
 		accelerometer.add_observer(observer);
-		context.checking(new Expectations(){{
-			oneOf(observer).handle_acceleration_report(-50);
-		}});
-	  
+		context.checking(new Expectations() {
+			{
+				oneOf(observer).handle_acceleration_report(-50);
+			}
+		});
+
 		accelerometer.report_acceleration(-50);
-    }
+	}
+
+	@Test
+	public void //
+	testAccelerometerCanRespondToFailureWhenReportingAcceleration() throws Exception {
+		Accelerometer accelerometer = new Accelerometer();
+		accelerometer.add_observer(observer);
+		context.checking(new Expectations() {
+			{
+				allowing(observer).handle_acceleration_report(-50);
+				will(throwException(new Exception()));
+			}
+		});
+
+		try {
+			accelerometer.report_acceleration(-50);
+			fail("No threw exception");
+		} catch (Exception e) {
+		}
+	}
 }
